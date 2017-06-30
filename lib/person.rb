281c26df9099e291eaf19beeb1512ca5565aa738
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-30 13:58:27 -0400

    added person class and instance methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,65 @@
 # your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :hygiene, :happiness
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(happiness)
+    happiness > 10 ? @happiness = 10 : (happiness < 0 ? @happiness = 0 : @happiness = happiness)
+  end
+
+  def hygiene=(hygiene)
+    hygiene > 10 ? @hygiene = 10 : (hygiene < 0 ? @hygiene = 0 : @hygiene = hygiene)
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(person)
+    self.happiness += 3
+    person.happiness += 3
+    "Hi #{person.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+    when "politics"
+      person.happiness -= 2
+      self.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      person.happiness += 1
+      self.happiness += 1
+      "blah blah sun blah rain"
+    else
+        "blah blah blah blah blah"
+    end
+  end
+end
